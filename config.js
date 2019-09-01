812affb3fdbf3ac11f2536f36a099bf418848ad2
Karolina Pietruszka <pietruszka.karolina94@gmail.com>
2019-09-01 15:30:35 +0200

    task finished

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '1043114392858-thanvolr1tlgol9rk6ev7iu0h8m46143.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'bg2aq-n1tv23aCWIA7gZbLkP',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
+
